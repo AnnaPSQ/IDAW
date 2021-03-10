ce788c368fe774d2a3f580f928956f5fe1cbe41b
AnnaPSQ <annapasquier4@gmail.com>
2021-03-10 14:07:32 +0100

    commit

---
+++ TP2/Exo3/infos_technique.php
@@ -0,0 +1,11 @@
+<?php
+require_once('template_header.php');
+?>
+    <?php
+    require_once('template_menu.php');
+    ?>
+    <h1> Cette page regroupe les infos techniques </h1>
+
+<?php
+require_once('template_footer.php');
+?>
--- TP2/Exo3/template_menu.php
+++ TP2/Exo3/template_menu.php
@@ -2,8 +2,8 @@
                 <ul>  
             
                 <li><a href="index.php">Accueil</a></div></li>
-                <li><a id="currentpage"
-                    href="cv.php">CV</a></div></li>
+                <li><a href="cv.php">CV</a></div></li>
                 <li><a href="hobbies.php">Hobbies</a></div></li>
+                <li><a href="infos_technique.php">Infos</a></div></li>
                 </ul>
-            </nav>
+</nav>
