1f6a3c80f0f76ff6dee128cf8e610902f53bb393
AnnaPSQ <annapasquier4@gmail.com>
2021-03-12 15:56:56 +0100

    commit

---
+++ TP2/Exo6/accueil.php
@@ -0,0 +1,3 @@
+<h2>Des renseignements sur ma personne disponibles sur ce site !</h1>
+<p> Trop sympa ! </p>
+<img src = "Images/the-tale-of-the-princess-kaguya-01.jpg"  alt = "Accueil" width = "300px">
