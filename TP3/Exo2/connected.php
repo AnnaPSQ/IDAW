98ffe5cc10b8d535724014e973f8498e517ef354
AnnaPSQ <annapasquier4@gmail.com>
2021-03-19 10:02:35 +0100

    commit

--- TP3/Exo2/connected.php
+++ TP3/Exo2/connected.php
@@ -3,6 +3,7 @@
     require_once("testConnexion.php");
     // on simule une base de données
     $users = $resultat;
+
     $login = "anonymous";
     $errorText = "";
     $successfullyLogged = false;
@@ -10,21 +11,22 @@
         $tryLogin=$_POST['login'];
         $tryPwd=$_POST['password'];
 
-        // si login existe et password correspond
-        if( array_key_exists($tryLogin,$users) && $users[$tryLogin]==$tryPwd ) {
-        $successfullyLogged = true;
-        $login = $tryLogin;
-        } 
-        else{
+        foreach($resultat as $numbers => $informations){
+            // si login existe et password correspond
+            if(($informations['login'] == $tryLogin) && ($informations['password']== $tryPwd)){
+                $successfullyLogged = true;
+                $login = $tryLogin;
+                $_SESSION['login'] = $tryLogin;
+            }
+        }
+
+        if($successfullyLogged = false){
             $errorText = "Erreur de login/password";
         }
     }
 
-    if(!$successfullyLogged) {
+    else{
+        $errorText = "Merci de remplir le formulaire";
         echo $errorText;
-    }
-    else {
-        $_SESSION['login'] = $tryLogin;
-
     }
 ?>
